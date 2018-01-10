747e0ea168d12f43f955847f18306ff30cffe4c7
Vitaly Hlapkovsky <vitaly.hlapkovsky@gmail.com>
2018-01-11 01:43:08 +0200

    correct code

--- credit.c
+++ credit.c
@@ -3,38 +3,87 @@
 
 int main(void)
 {
-    // Asking user for height and checking it's range
-    int h, i, j;
+    long number, checkNumber;
+    int count, i, everySecondNumber, checksum, checksumNow, everyFirstNumber, valid, masterCheck, visaCheck, amexCheck;
+    bool ccNotFound = true;
+
+
+    // Asking user for card number which is > 0
     do
     {
-        h = get_int("Height: ");
+        number = get_long_long("Number: ");
     }
-    while (h <0 || h > 23);
+    while (number <= 0);
 
-    // Building the piramide
-    for (i = 0; i < h; i++)
-    {
-        // Printing spaces
-        for (j = 0; j < h - i - 1; j++)
+    // Counting digits in number
+    checkNumber = number;
+    while (checkNumber > 0)
         {
-            printf(" ");
+            checkNumber = checkNumber / 10;
+            count = count + 1;
         }
-        // Printing # - left row
-        for (j = 0; j < i + 1; j++)
+
+    // Checking lenght, if not 13, 15 or 16 - instant INVALID
+    if (count < 13 || count > 16 || count == 14)
         {
-            printf("#");
+            printf("INVALID\n");
         }
-        // Printing two spaces between rows
-        for (j = 0; j < 2; j++)
+
+    // Main calculations
+    if (count == 13 || count == 15 || count == 16)
         {
-            printf(" ");
+
+            for (i = 0; i < 8; i++) // Runnung it eight times
+            {
+                everyFirstNumber = number % 10;             // Extracting every first digit from the end
+                everySecondNumber = (number / 10) % 10;     // Extracting every second digit from the end
+                number /= 100;                              // Card number losing two last digits
+
+                checksumNow = everySecondNumber * 2;        // First step of checksum calculation - *2
+                if (checksumNow > 9)                        // Checking if result more then one digit
+                    {
+                    checksumNow =
+                        ((checksumNow / 10) % 10) + (checksumNow % 10); // Addition of the first and second digits
+                    }
+
+                // Checking company identifiers
+                if (number >= 4 && number <= 999)
+                    {
+                    amexCheck = (number / 10);              // American Express: 15, should start from 34 or 37
+                    masterCheck = number;                   // Master Card: 16, should start from 51, 52, 53, 54 or 55
+                    if (count == 16)
+                    {
+                        visaCheck = (number / 10) % 10;         // Visa: 16...
+                    } else {
+                        visaCheck = number % 10;                // ..or 13, should start from 4
+                    }}
+
+                // Checksum total calculation
+                checksum = checksum + checksumNow + everyFirstNumber;
+
+            }
+
+            // Validating and printing result
+            valid = checksum % 10;      // Should be zero
+            if (valid == 0 && amexCheck == 37) // Checks for American Express
+                {
+                    printf("AMEX\n");
+                    ccNotFound = false;
+                }
+            if (valid == 0 && masterCheck >= 51 && masterCheck <= 55) // Checks for Master
+                {
+                    printf("MASTERCARD\n");
+                    ccNotFound = false;
+                }
+            if (valid == 0 && visaCheck == 4)               // Checks for Visa
+                {
+                    printf("VISA\n");
+                    ccNotFound = false;
+                }
+            if (ccNotFound == true)                         // If all checks failed
+                {
+                printf("INVALID\n");
+                }
         }
-        // Printing # - right row
-        for (j = 0; j < i + 1; j++)
-        {
-            printf("#");
-        }
-        // New row
-        printf("\n");
-    }
+    return 0;
 }
